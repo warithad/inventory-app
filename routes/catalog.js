68347cff7f657e839238e9f7955249e61cd4a358
warith <warith.ad02@gmail.com>
2023-02-19 23:55:27 +0100

    Rearrange routes for proper routing

--- routes/catalog.js
+++ routes/catalog.js
@@ -9,8 +9,6 @@ router.get('/', item_controller.items_list_get);
 
 router.get('/items', item_controller.items_list_get);
 
-router.get('/item/:id', item_controller.item_detail_get);
-
 router.get('/item/create', item_controller.item_create_get);
 
 router.post('/item/create', item_controller.item_create_post);
@@ -23,10 +21,10 @@ router.get('/item/:id/delete', item_controller.item_delete_get);
 
 router.post('/item/:id/delete', item_controller.item_delete_post);
 
+router.get('/item/:id', item_controller.item_detail_get);
+
 
 router.get('/categories', category_controller.categories_list_get);
-
-router.get('/category/:id', category_controller.category_detail_get);
 
 router.get('/category/create', category_controller.category_create_get);
 
@@ -39,5 +37,7 @@ router.post('/category/:id/update', category_controller.category_update_post);
 router.get('/category/:id/delete', category_controller.category_delete_get);
 
 router.post('/category/:id/delete', category_controller.category_delete_post);
+
+router.get('/category/:id', category_controller.category_detail_get);
 
 module.exports = router;
