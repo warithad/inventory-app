317a285e23a65c1cbfa15c3b644d9db9f1e89314
warith <warith.ad02@gmail.com>
2023-02-19 23:56:34 +0100

    Implement post and get request of category_create

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,8 @@
 const Category = require('../models/category')
 const Item = require('../models/item')
 const async = require('async')
+const {body, validationResult} = require('express-validator');
+
 
 // /categories
 exports.categories_list_get = (req, res, next) =>{
@@ -48,12 +50,45 @@ exports.category_detail_get = (req, res, next) => {
 }
 
 exports.category_create_get = (req, res) =>{
-    res.send('NOT IMPLEMENTED: item category GET');
+    res.render('category_form', {title: 'Create Category'});
 }
 
-exports.category_create_post = (req, res) =>{
-    res.send('NOT IMPLEMENTED: item category POST');
-}
+exports.category_create_post = [
+    body('name', 'Category name required').trim().isLength({min: 1}).escape(),
+
+    (req, res, next) =>{
+        const errors = validationResult(req);
+
+        const category = new Category({name: req.body.name});
+
+        if(!errors.isEmpty()){
+            res.render('category_form', {
+                title: 'Create Category',
+                category,
+                errors: errors.array()
+            });
+            return;
+        }
+        else {
+            Category.findOne({name: req.body.name}).exec((err, found_category) =>{
+                if(err){
+                    return next(err);
+                }
+                if(found_category){
+                    res.redirect(found_category.url);
+                } else {
+                    category.save(err =>{
+                        if(err) {
+                            return next(err);
+                        }
+                        res.redirect(category.url);
+                    })
+                }
+            })
+        }
+    }
+]
+
 
 exports.category_update_get = (req, res) =>{
     res.send('NOT IMPLEMENTED: item category GET');
